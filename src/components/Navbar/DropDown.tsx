e0e590cbee5341816b8381d19cbe39b2530f1944
M_HASNAIN_ALI_SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-09-23 22:36:35 +0500

    added delete icon on navbar

--- src/components/Navbar/DropDown.tsx
+++ src/components/Navbar/DropDown.tsx
@@ -1,20 +1,43 @@
 "use client";
-import { signOut } from "next-auth/react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import useChatStore from "@/store/useChatStore";
+import { Delete, Trash } from "lucide-react";
 
 const DropDown = () => {
-  const handleLogout = async () => {
-    await signOut({ callbackUrl: "/" });
-  };
+  const { selectedChat } = useChatStore();
 
   return (
     <DropdownMenu>
+      {selectedChat && (
+        <>
+          <DropdownMenuTrigger asChild>
+            <div className="w-7 h-7 rounded-full flex items-center justify-center cursor-pointer">
+              <span className="text-white text-2xl font-bold pb-2.5">...</span>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-56 bg-red-500 border border-red-600 hover:border-red-700 hover:bg-red-700"
+            align="end"
+          >
+            <DropdownMenuItem className="cursor-pointer hover:bg-red-700 text-white transition duration-200">
+              <Trash className="text-white" /> Delete Chat
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </>
+      )}
+    </DropdownMenu>
+  );
+};
+
+export default DropDown;
+
+{
+  /* <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div className="w-7 h-7 rounded-full bg-gray-200 flex items-center justify-center cursor-pointer">
           <span className="text-gray-600 font-semibold">U</span>
@@ -28,8 +51,5 @@ const DropDown = () => {
           <i className="ri-logout-box-r-line"></i> Log out
         </DropdownMenuItem>
       </DropdownMenuContent>
-    </DropdownMenu>
-  );
-};
-
-export default DropDown;
+    </DropdownMenu> */
+}
